57604a11f004d0a924e4e9d0e15808962cb00816
Diego Durán <diego.duran.rguez@gmail.com>
2022-01-30 00:55:49 +0100

    fix(*): fixed and improved some features

---
+++ src/config/nominations.config.ts
@@ -0,0 +1,10 @@
+export default {
+  nominations: {
+    rafa: '.answer-wrapper:nth-child(1) > .wave-wrapper > button',
+    carmen: '.answer-wrapper:nth-child(2) > .wave-wrapper > button',
+    elena: '.answer-wrapper:nth-child(3) > .wave-wrapper > button',
+    alatzne: '.answer-wrapper:nth-child(4) > .wave-wrapper > button',
+  },
+  url: 'https://www.telecinco.es/secret-story/vota-segundo-expulsado_18_3273123698.html',
+  chooseTo: 'elena'
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as puppeteer from 'puppeteer';
 import puppeteerConfig from "./config/puppetter.config";
 import deviceConfig from "./config/device.config";
+import nominationsConfig from "./config/nominations.config";
 
 import { logger } from './util/logger';
 
@@ -11,11 +12,9 @@ try {
 
       // @ts-ignore
       const browser = await puppeteer.launch(puppeteerConfig);
-      const context = await browser.createIncognitoBrowserContext();
       logger({severity: 'info', message: 'Initializing browser options'});
 
       let page = await browser.newPage();
-      // await blockImages(page);
 
       // Configure the navigation timeout
       await page.setDefaultNavigationTimeout(0);
@@ -24,39 +23,42 @@ try {
       await page.emulate(deviceConfig);
 
       try {
-        logger({severity: 'info', message: 'Going to Secret Story...'});
-        const url = 'https://app.ex.co/stories/item/5dd77e8b-82af-4e1f-9818-bac724b81844?feed=true&implementation=amp&src=%2F%2Fapp.ex.co%2Fstories%2Fitem%2F5dd77e8b-82af-4e1f-9818-bac724b81844&embedBy=00000000-0000-0000-0000-000000000000&game=%2Fstories%2Fitem%2F5dd77e8b-82af-4e1f-9818-bac724b81844&useComments=false&gameInfo=false&useShares=false&socialReferrer=false&height=auto&parentUrl=https%3A%2F%2Fwww.telecinco.es%2Fsecret-story%2Fvota-repesca_18_3229550141.html&parentHost=www.telecinco.es';
+        const url = nominationsConfig.url;
+        logger({severity: 'info', message: `Going to Secret Story nomination page (${nominationsConfig.url})`});
+        logger({severity: 'info', message: `Selected person ${nominationsConfig.chooseTo}`});
         await page.goto(url, { waitUntil: 'load' });
-        await page.waitForSelector('iframe', { visible: true });
-        const emmy = '.poll-answer-container:nth-child(1) > .answer-button-wrapper > button';
-        const fiama = '.poll-answer-container:nth-child(2) > .answer-button-wrapper > button';
-        const frigenti = '.poll-answer-container:nth-child(3) > .answer-button-wrapper > button';
-        const lucia = '.poll-answer-container:nth-child(4) > .answer-button-wrapper > button';
-        const adara = '.poll-answer-container:nth-child(5) > .answer-button-wrapper > button';
+        await page.waitForSelector('#didomi-notice-agree-button', { visible: true });
 
-        const frame = page.frames().find(frame => frame.url() !== url || frame.url() === 'about:blank');
+        // @ts-ignore
+        const selectedPerson = nominationsConfig.nominations[nominationsConfig.chooseTo];
+        logger({severity: 'info', message: `Selected person button ${selectedPerson}`});
+
+        const elementHandle = await page.$('iframe[data-device="desktop"]');
+        // @ts-ignore
+        const frame = await elementHandle.contentFrame();
 
         logger({severity: 'info', message: 'Clicking...'});
-        await frame.click(adara);
+
+        // @ts-ignore
+        await frame.click(selectedPerson);
         logger({severity: 'success', message: 'Clicked...'});
 
         await page.waitForTimeout(5000);
-        // await frame.waitForSelector('.poll-answer-container .selected', { visible: true });
 
         setTimeout(async () => {
           logger({severity: 'success', message: 'Closing...'});
           await page.close();
           await browser.close();
         }, 1000);
-      } catch (error) {
+      } catch (err: unknown) {
         await page.screenshot({ path: `outputs/${Date.now()}.png` });
         logger({severity: 'error', message: 'There was an error'});
-        logger({severity: 'error', message: error.message});
+        logger({severity: 'error', message: (err as Error).message});
         logger({severity: 'error', message: 'Exiting'});
         process.exit(999);
       }
     })();
-  }, 10000);
+  }, 60000);
 } catch (err) {
   logger({severity: 'error', message: 'Unknown fatal error, exiting!'});
   process.exit(1);
